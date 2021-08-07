cc1614c1040a185e21a27554f450e5983813241f
Nerwin <nerwintjen@gmail.com>
2021-08-07 12:54:07 +1000

    Commit 2 , add some inputs to the testing file and change the method in main to adhere to the test instructions

--- src/word/count/nerwin/com/Main.java
+++ src/word/count/nerwin/com/Main.java
@@ -1,12 +1,11 @@
 package word.count.nerwin.com;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 import word.count.nerwin.com.filter.CharacterFilter;
 import word.count.nerwin.com.filter.LengthFilter;
@@ -18,27 +17,41 @@ public class Main {
 	 */
 	
 	public static void main(String[]args) {
-		List<String> stringListInput = Arrays.asList("Menu", "manly","jnasiu23","2343","asdasd","kjnasjdnakjsdnkjs");
-		LengthFilter a = new LengthFilter();
-		CharacterFilter b = new CharacterFilter();
-		CountWords countwords1 = new CountWords(stringListInput);
-		CountWords countwords2 = new CountWords(stringListInput);
-		a.moreThanAndEqualTo(8).lessThanAndEqualTo(10);
-		b.comparingCharacter(0, "ma").withCaseSensitivity(true);
-		countwords1.addFilter(a);
-		countwords2.addFilter(b);
-		System.out.println("List of filtered count words : "+ countwords1.filteredWordCount());
-		countwords1.getListOfStrings().forEach(System.out::println);
-		System.out.println("List of filtered count words : "+ countwords2.filteredWordCount());
-		countwords2.getListOfStrings().forEach(System.out::println);
-		
-		Path path = Paths.get("InputFile.txt");
-		 try (Stream<String> stream = Files.lines(path)) {
-	            stream.forEach(System.out::println);
+		//read from InputFile.txt
+		List<String> stringListInput = new ArrayList<String>();
+		 try  {
+			 	stringListInput = Files.readAllLines(Paths.get("InputFile.txt"), StandardCharsets.UTF_8);
 	        }
 	        catch (IOException e) {
 	            e.printStackTrace();
 	        }
+		 stringListInput.forEach(System.out::println);
+		 //case 1 count and returns the number of words that start with "M" or "m"		 
+		 CountWords countwords1 = new CountWords(stringListInput);
+		 CharacterFilter charFilter = new CharacterFilter();
+		 charFilter.comparingCharacter(0, "m")
+		 		   .withCaseSensitivity(false);
+		 countwords1.addFilter(charFilter);
+		 System.out.printf("Number of words that starts with the letter \"m\" or \"M\" is %s",countwords1.filteredWordCount());
+		 
+		 //case 2 returns all words longer than 5 character
+		 CountWords countwords2 = new CountWords(stringListInput);
+		 LengthFilter lengthFilter = new LengthFilter();
+//		 
+//			List<String> stringListInput = 
+//			LengthFilter a = new LengthFilter();
+//			CharacterFilter b = new CharacterFilter();
+//			CountWords countwords1 = new CountWords(stringListInput);
+//			CountWords countwords2 = new CountWords(stringListInput);
+//			a.moreThanAndEqualTo(8).lessThanAndEqualTo(10);
+//			b.comparingCharacter(0, "ma").withCaseSensitivity(true);
+//			countwords1.addFilter(a);
+//			countwords2.addFilter(b);
+//			System.out.println("List of filtered count words : "+ countwords1.filteredWordCount());
+//			countwords1.getListOfStrings().forEach(System.out::println);
+//			System.out.println("List of filtered count words : "+ countwords2.filteredWordCount());
+//			countwords2.getListOfStrings().forEach(System.out::println);
+		
 		
 	}
 }
